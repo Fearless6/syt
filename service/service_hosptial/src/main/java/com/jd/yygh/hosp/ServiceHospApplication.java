b5bc08e974acb22bf98f56740a7fc033c720dc78
一个瘦瘦的胖子 <1284937301@qq.com>
2022-06-17 00:12:05 +0800

    初次提交代码

---
+++ service/service_hosptial/src/main/java/com/jd/yygh/hosp/ServiceHospApplication.java
@@ -0,0 +1,15 @@
+package com.jd.yygh.hosp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 一个胖胖的瘦子
+ * @date 2022/6/12
+ */
+@SpringBootApplication
+public class ServiceHospApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHospApplication.class, args);
+    }
+}
---
+++ service/service_hosptial/src/main/java/com/jd/yygh/hosp/mapper/HospitalSetMapper.java
@@ -0,0 +1,13 @@
+package com.jd.yygh.hosp.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jd.yygh.model.hosp.HospitalSet;
+
+/**
+ * @author 一个胖胖的瘦子
+ * @date 2022/6/12
+ */
+public interface HospitalSetMapper extends BaseMapper<HospitalSet> {
+
+}
